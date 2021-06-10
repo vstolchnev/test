fbb198fb1628a4d7a5b2ac2ee108620cf52a6fd4
Artem <you@example.com>
2021-06-10 20:45:01 +0300

    В main добавлен fishing

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from animals import Animals
 from numbers import Numbers
+from Fish import fishing
 
 def main():
 	crocodile = Animals('крокодил', 'зеленый', '22')
 	crocodile.get_info()
 	num = Numbers()
-	num.info()
+	num.info() 
+    fishing = fishing()
+    fishing.getFish()
 
 
 if __name__ == "__main__":
