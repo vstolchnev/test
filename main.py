77692c9d2710ef78e845561e8795d9221f67c600
OlegKatrinets <Oleg.Katrinets@gmail.com>
2021-06-10 20:34:21 +0300

    Insects added

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from animals import Animals
 from numbers import Numbers
+from insects import Insects
 
 def main():
 	crocodile = Animals('крокодил', 'зеленый', '22')
 	crocodile.get_info()
 	num = Numbers()
 	num.info()
+    bee = Insects('bee', true)
+    bee.walk
 
 
 if __name__ == "__main__":
