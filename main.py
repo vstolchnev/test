46ff37a13bdce5db40148475915b4c1ce16b37b1
FeNix784 <you@example.com>
2021-06-10 20:56:20 +0300

    Добавил игральные карты.

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from animals import Animals
 from numbers import Numbers
+from PlayingCards import Playing_Cards
 
 def main():
 	crocodile = Animals('крокодил', 'зеленый', '22')
 	crocodile.get_info()
 	num = Numbers()
 	num.info()
-
+    trump_card = Playing_Cards()
+    trump_card.get_trump_card()
 
 if __name__ == "__main__":
 	main()
