f0adc8ccafdd68a689877c8945dafddcfa5eab5c
lunnex <ilyakorotkih62@gmail.com>
2021-06-10 20:24:31 +0300

    Задание

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from animals import Animals
 from numbers import Numbers
+from Plants import Plants
+
 
 def main():
+	plants = Plants()
+	plants.writing
 	crocodile = Animals('крокодил', 'зеленый', '22')
 	crocodile.get_info()
 	num = Numbers()
