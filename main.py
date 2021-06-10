9666fb57b20f2748604132edc649fffa826a393d
tester <you@example.com>
2021-06-10 21:00:25 +0300

    111

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from animals import Animals
 from numbers import Numbers
-from shoes import Shoes
+from main_shoes import shoes
 
 def main():
 	crocodile = Animals('крокодил', 'зеленый', '22')
 	crocodile.get_info()
 	num = Numbers()
 	num.info()
-
+    shoes = shoes()
+    shoes.get_shoes()
 
 if __name__ == "__main__":
 	main()
