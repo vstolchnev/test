fe568f855ffdf923786eb62cc649e9b501a29012
Rogovastov A.Y <h-bomb@bk.ru>
2022-04-25 18:25:34 +0300

    Добавлен Mixer

--- Runner.py
+++ Runner.py
@@ -1,10 +1,11 @@
 import ProductItemModule
-
+import Mixer
 ItemList = []
 
 TestItem = ProductItemModule.ProductItem
-
+Mixer = Mixer.Mixer
 ItemList.append(TestItem(0,"0000","TestItem",0))
+ItemList.append(Mixer(12, "a0012bc", "Mixer", 1500, 800))
 
 for item in ItemList:
     print(item.get_info())
