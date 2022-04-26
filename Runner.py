8c6b37897b9b97cba8766a653c1c51cd46dd9b2b
apuvo <apuvo@example.com>
2022-04-26 17:40:42 +0500

    Добавил класс принтера

--- Runner.py
+++ Runner.py
@@ -1,9 +1,13 @@
 import HairDryerModule
+import PrinterModule
+
 ItemList = []
 
 HairDryer = HairDryerModule.Hairdryer
+Printer = PrinterModule.Printer
 
 ItemList.append(HairDryer(12, "a0012bc", "Hairdryer", 1500, 800))
+ItemList.append(Printer(14, "PRNTRLSR300", "Laser Printer", 3300.0, 300))
 
 for item in ItemList:
     print(item.get_info())
